cff612f0803478f7f103d70631d27ec36dfa4e52
Mike Garrity <garritymichael@protonmail.com>
2022-11-26 15:58:47 -0600

    testing mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-black text-secondary absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-black text-secondary absolute top-20 right-2 mx-4 my-2 min-w-[140px] z-50 sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
