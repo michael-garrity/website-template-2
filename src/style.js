2536034ae889dae3d458e7a15a015094bee6c272
Mike Garrity <garritymichael@protonmail.com>
2022-11-26 15:52:28 -0600

    fixing mobile nav

--- src/style.js
+++ src/style.js
@@ -4,7 +4,7 @@ const styles = {
     heading: "font-montserrat font-bold xs:text-[48px] text-[40px] text-secondary xs:leading-[76.8px] leading-[66.8px] w-full",
     heading2: "font-montserrat font-bold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
     paragraph: "font-montserrat font-normal text-black text-[18px] leading-[30.8px] z-50",
-    paragraph2: "font-montserrat font-normal text-secondary text-[18px] leading-[30.8px] z-50",
+    paragraph2: "font-montserrat font-normal text-secondary text-[18px] leading-[30.8px] z-10",
   
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start md:mt-[-50px]",
